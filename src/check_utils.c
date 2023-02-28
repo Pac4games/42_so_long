fc217e92f12c07829e4234b3b8347c04fd27361d
Pac4games <amanoskull42@gmail.com>
2023-02-28 23:50:11 +0000

    Added player and exit checkers.

--- src/check_utils.c
+++ src/check_utils.c
@@ -6,7 +6,7 @@
 /*   By: paugonca <paugonca@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/27 21:16:02 by paugonca          #+#    #+#             */
-/*   Updated: 2023/02/28 23:12:14 by paugonca         ###   ########.fr       */
+/*   Updated: 2023/02/28 23:44:54 by paugonca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ static int	check_shape(char **map, int p, int i)
 	while (map[p])
 		if (ft_strlen(map[p++]) != i)
 			return (0);
-	if (p - 1 < 3 || i - 1 < 3)
+	if (p < 3 || i - 1 < 3)
 		return (0);
 	return (1);
 }
@@ -44,11 +44,51 @@ static int	check_walls(char **map, int p, int i)
 	return (1);
 }
 
+static int	check_player(char **map, int p, int i)
+{
+	int	player;
+
+	player = 0;
+	while (map[p])
+	{
+		i = 0;
+		while (map[p][i])
+			if (map[p][i++] == 'P')
+				player++;
+		p++;
+	}
+	if (player != 1)
+		return (0);
+	return (1);
+}
+
+static int	check_exit(char **map, int p, int i)
+{
+	int	exit;
+
+	exit = 0;
+	while (map[p])
+	{
+		i = 0;
+		while (map[p][i])
+			if (map[p][i++] == 'E')
+				exit++;
+		p++;
+	}
+	if (exit != 1)
+		return (0);
+	return (1);
+}
+
 void	check_map(char **map)
 {
 	if (!check_shape(map, 0, 0))
 		print_error("provided map is not rectangular or too small.\n");
 	else if (!check_walls(map, 0, 0))
 		print_error("provided map not surrounded by walls.\n");
+	else if (!check_player(map, 1, 0))
+		print_error("provided map has zero or multiple starting positions.\n");
+	else if (!check_exit(map, 1, 0))
+		print_error("provided map contains zero or multiple exits\n");
 	ft_putstr_fd("The provided map is valid.\n", 1);
 }
