43f57d2276ef9d807ede5914a56bb263c16e3b81
Paulo Cesar Goncalves Nicolau <paugonca@c2r8s6.42lisboa.com>
2023-03-03 12:30:13 +0000

    Norme error fix.

--- src/window_utils.c
+++ src/window_utils.c
@@ -6,7 +6,7 @@
 /*   By: paugonca <paugonca@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/01 15:35:03 by paugonca          #+#    #+#             */
-/*   Updated: 2023/03/01 22:18:56 by paugonca         ###   ########.fr       */
+/*   Updated: 2023/03/03 12:05:39 by paugonca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,25 @@
 
 static void	window_set_tile(char tile, int x, int y)
 {
+	if (tile == '1')
+		(*window()).img = mlx_xpm_file_to_image((*window()).mlx, WALL,
+				&(*window()).img_x, &(*window()).img_y);
+	else if (tile == '0')
+		(*window()).img = mlx_xpm_file_to_image((*window()).mlx, FLOOR,
+				&(*window()).img_x, &(*window()).img_y);
+	else if (tile == 'P')
+		(*window()).img = mlx_xpm_file_to_image((*window()).mlx,
+				PLAYER_S, &(*window()).img_x, &(*window()).img_y);
+	else if (tile == 'C')
+		(*window()).img = mlx_xpm_file_to_image((*window()).mlx,
+				COLLECTIBLE, &(*window()).img_x, &(*window()).img_y);
+	else if (tile == 'E')
+		(*window()).img = mlx_xpm_file_to_image((*window()).mlx,
+				EXIT, &(*window()).img_x, &(*window()).img_y);
+	else if (tile != '\n')
+		print_error("invalid object detected in map.");
 	mlx_put_image_to_window((*window()).mlx, (*window()).win, (*window()).img,
-			x * 64, y * 64);
+		x * 64, y * 64);
 }
 
 static void	window_load(char **map, int p, int i)
@@ -25,23 +42,6 @@ static void	window_load(char **map, int p, int i)
 		i = 0;
 		while (map[p][i])
 		{
-			if (map[p][i] == '1')
-				(*window()).img = mlx_xpm_file_to_image((*window()).mlx, WALL,
-						&(*window()).img_x, &(*window()).img_y);
-			else if (map[p][i] == '0')
-				(*window()).img = mlx_xpm_file_to_image((*window()).mlx, FLOOR,
-						&(*window()).img_x, &(*window()).img_y);
-			else if (map[p][i] == 'P')
-				(*window()).img = mlx_xpm_file_to_image((*window()).mlx,
-						PLAYER_S, &(*window()).img_x, &(*window()).img_y);
-			else if (map[p][i] == 'C')
-				(*window()).img = mlx_xpm_file_to_image((*window()).mlx,
-						COLLECTIBLE, &(*window()).img_x, &(*window()).img_y);
-			else if (map[p][i] == 'E')
-				(*window()).img = mlx_xpm_file_to_image((*window()).mlx,
-						EXIT, &(*window()).img_x, &(*window()).img_y);
-			else if (map[p][i] != '\n')
-				print_error("invalid object detected in map.");
 			window_set_tile(map[p][i], i, p);
 			i++;
 		}
@@ -54,7 +54,7 @@ void	window_create(char **map)
 	(*window()).size_x = map_size(map, 'x');
 	(*window()).size_y = map_size(map, 'y');
 	(*window()).mlx = mlx_init();
-	(*window()).win = mlx_new_window((*window()).mlx, (*window()).size_x * 64, 
+	(*window()).win = mlx_new_window((*window()).mlx, (*window()).size_x * 64,
 			(*window()).size_y * 64, "Silver Rush");
 	window_load(map, 0, 0);
 	mlx_loop((*window()).mlx);
