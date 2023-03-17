efdad3cc310cbddc7e7acb0e6610f82747f27a47
Pac4games <amanoskull42@gmail.com>
2023-03-17 19:25:12 +0000

    Made some changes ig

--- src/check_path.c
+++ src/check_path.c
@@ -6,14 +6,33 @@
 /*   By: paugonca <paugonca@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/16 15:35:20 by paugonca          #+#    #+#             */
-/*   Updated: 2023/03/17 18:08:45 by paugonca         ###   ########.fr       */
+/*   Updated: 2023/03/17 19:24:54 by paugonca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
-/*
-void	check_path(char **map, t_pos pos, int collet)
+
+static void	find_path(char **tmp, t_pos pos, int collet)
 {
 
 }
-*/
+
+void	check_path(char **map)
+{
+	char	**tmp;
+	int		p;
+	
+	p = 0;
+	while (map[p])
+		p++;
+	tmp = malloc(p * sizeof(char *) + 1);
+	p = 0;
+	while (map[p])
+	{
+		tmp[p] = ft_strdup(map[p]);
+		p++;
+	}
+	if (!tmp)
+		return ;
+	find_path(tmp, get_player_pos(map), count_collet(map));
+}
