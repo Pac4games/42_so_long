0c04cb677c8d3a645298f9ebcf2eb30f40f84e0e
Pac4games <amanoskull42@gmail.com>
2023-03-18 13:42:09 +0000

    Cleaned up the find_path function a bit

--- src/check_path.c
+++ src/check_path.c
@@ -6,7 +6,7 @@
 /*   By: paugonca <paugonca@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/16 15:35:20 by paugonca          #+#    #+#             */
-/*   Updated: 2023/03/18 11:18:52 by paugonca         ###   ########.fr       */
+/*   Updated: 2023/03/18 13:41:22 by paugonca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,35 +30,19 @@ static int	count_collet(char **map)
 	}
 	return (count);
 }
-/*
-static void	find_path(char **tmp, int x, int y, int collet)
-{
-	if (tmp[y][x] == 'C' || tmp[y][x] == 'E')
-		collet++;
-	tmp[y][x] = 'R';
-	if (tmp[y][x + 1] != '1' || tmp[y][x + 1] != 'R')
-		find_path(tmp, x + 1, y, collet);
-	if (tmp[y][x - 1] != '1' || tmp[y][x - 1] != 'R')
-		find_path(tmp, x - 1, y, collet);
-	if (tmp[y + 1][x] != '1' || tmp[y + 1][x] != 'R')
-		find_path(tmp, x, y + 1, collet);
-	if (tmp[y - 1][x] != '1' || tmp[y - 1][x] != 'R')
-		find_path(tmp, x, y - 1, collet);
-}
-*/
 
-void	find_path(char **tmp, int x, int y, int *c)
+static void	find_path(char **tmp, int x, int y, int *collet)
 {
-	*c += ('E' == tmp[y][x] || 'C' == tmp[y][x]);
+	*collet += (tmp[y][x] == 'C' || tmp[y][x] == 'E');
 	tmp[y][x] = 'R';
-	if ('1' != tmp[y][x + 1] && 'R' != tmp[y][x + 1])
-		find_path(tmp, x + 1, y, c);
-	if ('1' != tmp[y][x - 1] && 'R' != tmp[y][x - 1])
-		find_path(tmp, x - 1, y, c);
-	if ('1' != tmp[y + 1][x] && 'R' != tmp[y + 1][x])
-		find_path(tmp, x, y + 1, c);
-	if ('1' != tmp[y - 1][x] && 'R' != tmp[y - 1][x])
-		find_path(tmp, x, y - 1, c);
+	if (tmp[y][x + 1] != '1' && tmp[y][x + 1] != 'R')
+		find_path(tmp, x + 1, y, collet);
+	if (tmp[y][x - 1] != '1' && tmp[y][x - 1] != 'R')
+		find_path(tmp, x - 1, y, collet);
+	if (tmp[y + 1][x] != '1' && tmp[y + 1][x] != 'R')
+		find_path(tmp, x, y + 1, collet);
+	if (tmp[y - 1][x] != '1' && tmp[y - 1][x] != 'R')
+		find_path(tmp, x, y - 1, collet);
 }
 
 void	check_path(char **map, int x, int y)
